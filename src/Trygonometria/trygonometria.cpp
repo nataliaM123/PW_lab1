6ad91b6cd209ade3d08184f5b1174f9fe3deffce
Student <student@ib.polsl.pl>
2017-02-27 11:58:14 +0100

    Drugie zadanie - POPRAWIONE

---
+++ src/Trygonometria/trygonometria.cpp
@@ -0,0 +1,9 @@
+#include "trygonometria.h"
+#include <math.h>
+double sin_degree(double degree) {
+	double radian=degree*pi/180.0;
+	return sin(radian);   //funkcja biblioteczna z math.h
+}
+double cos_degree(double degree) {
+/*...*/
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#ifdef USE_TRIGONOMETRY_DEGREE
+result = cos_degree(45.0);
+#else
+result = cos(pi/4.0);
+#endif
 int main()
 {
 return 0;
